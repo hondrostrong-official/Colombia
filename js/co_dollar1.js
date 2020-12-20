c1ddd610c284df44c610adf38902412b609b2d31
hondrostrong-official <76270734+hondrostrong-official@users.noreply.github.com>
2020-12-20 04:03:28 +0200

    2

---
+++ js/co_dollar1.js
@@ -0,0 +1,18 @@
+﻿(function() {
+    "use strict";
+    window.countryList = {
+        co: {
+            name: "Colombia",
+            code: "co",
+            price: "119.900",
+            oldPrice: "239.800",
+            labelPrice: "$",
+            phoneHelper: "Por ejemplo, +57 12345678",
+            nameHelper: "Alejandro Rodriguez",
+            nameError: "Nombre (campo obligatorio)",
+            phoneError: "Debe introducir un teléfono",
+            countryError: "Debe introducir un país",
+			isDefault: true       
+        }
+    };
+})();
